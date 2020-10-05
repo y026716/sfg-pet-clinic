34c1b77d0c1a133485eafa1982762998a89e1f04
JBoysen <y026716@gmail.com>
2020-10-05 15:52:07 -0500

    Refactord Map Impl to abstract ID generation - Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
@@ -14,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    @RequestMapping({"","/","/index","/index.html"})
     public  String listVets(Model model){
 
         model.addAttribute("vets",vetService.findAll());
