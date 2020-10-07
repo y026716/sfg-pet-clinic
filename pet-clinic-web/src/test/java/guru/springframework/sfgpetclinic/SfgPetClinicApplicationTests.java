e65c00407ffc51c9b1f6ddc6c5f4751844f0cb64
JBoysen <y026716@gmail.com>
2020-10-07 16:49:21 -0500

    Added wro4j plugin and config from Spring Pet Clinic - Closes #24

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SfgPetClinicApplication.class)
 class SfgPetClinicApplicationTests {
 
 	@Test
