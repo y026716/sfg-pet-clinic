649c8621b309e8b08f765fa02a7f225c3be4eb23
JBoysen <y026716@gmail.com>
2020-10-29 02:47:01 -0500

    Added Builder pattern to PetType.  #56

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class PetControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1l).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1l).name("Dog").build());
+        petTypes.add(PetType.builder().id(2l).name("Cat").build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+    }
+
+}
